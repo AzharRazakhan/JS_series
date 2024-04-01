5c385ab8c4494a7a59006ed3b0635b22be05ced7
AzharRazakhan <46918499+AzharRazakhan@users.noreply.github.com>
2024-04-01 10:32:41 +0000

    "dataTypes"

---
+++ 02_basic/dataType.js
@@ -0,0 +1,10 @@
+//Primitive
+let age = 23; /*type Number*/
+let name = 'xyx'; /*type String*/ 
+let isActive = 23; /*type boolean*/ 
+let value; /*type undefined*/
+/*type Null*/
+//Non-Primitive
+
+//Array
+//Object
